349fcee7ac5851a99fee9cf43e3f4e0475d05511
Khalil Santana <khalilsantana@hotmail.com>
2023-11-16 14:35:48 -0300

    Base C Code

---
+++ Matrix.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+
+#define MATRIX_ROWS 3
+#define MATRIX_COLUMNS 3
+
+void printMatrix(int matrix[MATRIX_ROWS][MATRIX_COLUMNS]) {
+    for (int i = 0; i < MATRIX_ROWS; i++) {
+        for (int j = 0; j < MATRIX_COLUMNS; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void matrixMult(int matrix_A[MATRIX_ROWS][MATRIX_COLUMNS],
+                int matrix_B[MATRIX_ROWS][MATRIX_COLUMNS],
+                int matrix_C[MATRIX_ROWS][MATRIX_COLUMNS]) {
+    for (int i = 0; i < MATRIX_ROWS; i++) {
+        for (int j = 0; j < MATRIX_COLUMNS; j++) {
+            matrix_C[i][j] = 0;
+            for (int k = 0; k < MATRIX_COLUMNS; k++) {
+                matrix_C[i][j] += matrix_A[i][k] * matrix_B[k][j];
+            }
+        }
+    }
+}
+
+
+int main() {
+    int rows = MATRIX_ROWS;
+    int cols = MATRIX_COLUMNS;
+    int matrix_A[MATRIX_ROWS][MATRIX_COLUMNS] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int matrix_B[MATRIX_ROWS][MATRIX_COLUMNS] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+    int matrix_C[MATRIX_ROWS][MATRIX_COLUMNS];
+    printMatrix(matrix_A);
+    printf("\nTIMES\n\n");
+    printMatrix(matrix_B);
+    matrixMult(matrix_A, matrix_B, matrix_C);
+    printf("\nRESULT\n\n");
+    printMatrix(matrix_C);
+    return 0;
+}
